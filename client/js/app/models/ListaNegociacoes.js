90ec1f650fbceaf6b4b92fe34c86459a034b23c1
Wagner.Araujo <araujowp@gmail>
2022-04-15 07:45:32 -0300

    callback

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,24 @@
 class ListaNegociacoes {
 
-    constructor() {
+    constructor(atualizador) {
         this._negociacoes = []
+        this._atualizador = atualizador;
     }
 
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
+        this._atualizador(this);
+        // Reflect.apply(this._atualizador, this._contexto,[this]);
     }
     
     get negociacoes() {
     
         return [].concat(this._negociacoes);
+    }
+
+    apagar(){
+        this._negociacoes = [];
+        this._atualizador(this);
+        // Reflect.apply(this._atualizador, this._contexto, [this]);
     }
 }
